e31ce29e9d5d4073f71b3f077f4c1b0659419f52
Valentin Udaltsov <udaltsov.valentin@gmail.com>
2026-04-14 18:09:24 +0300

    Fix index.php types

--- examples/Project/index.php
+++ examples/Project/index.php
@@ -4,12 +4,14 @@ declare(strict_types=1);
 
 namespace Project;
 
+use Project\Thesis\HttpServerModule\Server;
 use Thesis\DIC;
 use function Amp\trapSignal;
 
 require_once __DIR__ . '/../../vendor/autoload.php';
 require_once __DIR__ . '/Thesis/HttpServerModule/methods.php';
 
+/** @var Server */
 $httpServer = DIC::init(new App());
 
 $httpServer->expose('0.0.0.0:1337');
